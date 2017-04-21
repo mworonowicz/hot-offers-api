3a2f992082875d4af46432e2358b57f59b96e7ed
Mateusz Czubak <czubakm@ryanair.com>
2017-04-21 14:51:12 +0200

    add user endpoint

--- src/main/scala/com/ryanair/hackathon/hotOffers/users/controller/UserDetailsController.scala
+++ src/main/scala/com/ryanair/hackathon/hotOffers/users/controller/UserDetailsController.scala
@@ -6,8 +6,7 @@ package com.ryanair.hackathon.hotOffers.users.controller
 import com.ryanair.hackathon.hotOffers.Controller
 import com.ryanair.hackathon.hotOffers.users.model.UserDetails
 import com.ryanair.hackathon.hotOffers.users.model.UsersJson._
-
-import scala.concurrent.Future
+import com.ryanair.hackathon.hotOffers.users.service.UserDetailsService
 
 trait UserDetailsController extends Controller {
   def route =
@@ -15,13 +14,15 @@ trait UserDetailsController extends Controller {
       pathEnd {
         post {
           entity(as[UserDetails]) { userDetails =>
-            println(userDetails)
-            val saved = Future.successful("saved")
-            onComplete(saved) { done =>
-              complete("userDetails saved")
+            val saved = UserDetailsService.saveDetails(userDetails)
+            onComplete(saved) { _ =>
+              complete(s"User ${userDetails.userId} saved")
             }
           }
-        }
+        } ~
+          get {
+            complete(UserDetailsService.getAll)
+          }
       }
     }
 }
