adadd809b6e406bb787ff6e8d7b13c0c73a4de1d
Woronowicz, Michal <woronowiczm@ryanair.com>
2017-04-22 11:14:34 +0200

    add websocket

--- src/main/scala/com/ryanair/hackathon/hotOffers/ApplicationContext.scala
+++ src/main/scala/com/ryanair/hackathon/hotOffers/ApplicationContext.scala
@@ -8,6 +8,7 @@ import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import com.ryanair.hackathon.hotOffers.airports.AirportContext
 import com.ryanair.hackathon.hotOffers.common.StreamSupervisionStrategy
 import com.ryanair.hackathon.hotOffers.common.httpClient.HttpClient
+import com.typesafe.sslconfig.akka.AkkaSSLConfig
 
 import scala.concurrent.ExecutionContext
 
@@ -15,6 +16,7 @@ trait ApplicationContext extends AirportContext {
   implicit val system: ActorSystem = ActorSystem("hot-offers-system")
   implicit val materializer: ActorMaterializer =
     ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(StreamSupervisionStrategy.decider))
+  val sslConfig = AkkaSSLConfig()
   implicit val executionContext: ExecutionContext = materializer.executionContext
   val httpClient = new HttpClient()
 
--- src/main/scala/com/ryanair/hackathon/hotOffers/offers/controller/WebSocketController.scala
+++ src/main/scala/com/ryanair/hackathon/hotOffers/offers/controller/WebSocketController.scala
@@ -1,15 +1,47 @@
 package com.ryanair.hackathon.hotOffers.offers.controller
 
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
 import akka.http.scaladsl.server.Route
-import com.ryanair.hackathon.hotOffers.Controller
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ActorAttributes, Supervision}
+import com.ryanair.hackathon.hotOffers.offers.model.OfferResult
+import com.ryanair.hackathon.hotOffers.offers.service.OfferService
+import com.ryanair.hackathon.hotOffers.users.service.UserDetailsService
+import com.ryanair.hackathon.hotOffers.{ApplicationContext, Controller}
+import spray.json._
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 trait WebSocketController extends Controller {
+  context: ApplicationContext =>
+  def getOffers =
+    Flow[Message].map {
+      case tm: TextMessage =>
+        tm.textStream.flatMapConcat(userId => {
+          println(userId)
+          Source.tick(0 seconds, 10 seconds, ())
+            .mapAsync(1)(_ =>
+              UserDetailsService.getAll
+                .find(_.userId == userId)
+                .fold[Future[OfferResult]](Future.failed(new RuntimeException("User does not exist"))) {
+                user =>
+                  OfferService.getCheapestOffers(user).run(context
+                    .httpClient)
+              })
+            .withAttributes(ActorAttributes.supervisionStrategy(Supervision.stoppingDecider))
+        }.recover {
+          case ex => TextMessage("user not found")
+        }).map(o => o.toJson.toString)
+      case bm: BinaryMessage =>
+        // ignore binary messages but drain content to avoid the stream being clogged
+        bm.dataStream.runWith(Sink.ignore)
+        Source.empty
+    }.flatMapConcat(identity).map(TextMessage(_))
 
   def route: Route =
-    path("test") {
-      get {
-        complete("ok")
-      }
+    path("offers") {
+      handleWebSocketMessages(getOffers)
     }
-
 }
