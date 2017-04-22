78f6979e479d713ecea4d7bfcd47c7d44d7c98f6
Mateusz Czubak <czubakm@ryanair.com>
2017-04-22 12:50:01 +0200

    enable cors

--- src/main/scala/com/ryanair/hackathon/hotOffers/Routing.scala
+++ src/main/scala/com/ryanair/hackathon/hotOffers/Routing.scala
@@ -4,12 +4,15 @@ import akka.http.scaladsl.server.{Directives, Route}
 import com.ryanair.hackathon.hotOffers.airports.controller.AirportController
 import com.ryanair.hackathon.hotOffers.offers.controller.WebSocketController
 import com.ryanair.hackathon.hotOffers.users.controller.UserDetailsController
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
 trait Routing extends AirportController with UserDetailsController with WebSocketController {
   application: ApplicationContext =>
 
-  override def route: Route = pathPrefix("hot-offers") {
-    super[WebSocketController].route ~ super[AirportController].route ~ super[UserDetailsController].route
+  override def route: Route = cors() {
+    pathPrefix("hot-offers") {
+      super[WebSocketController].route ~ super[AirportController].route ~ super[UserDetailsController].route
+    }
   }
 }
 
