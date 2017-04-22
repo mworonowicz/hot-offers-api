1d873e2fa45989082af0b29f918dd0300de8b9bc
Mateusz Czubak <czubakm@ryanair.com>
2017-04-22 14:56:48 +0200

    extend userdetails

--- src/main/scala/com/ryanair/hackathon/hotOffers/users/model/UserDetails.scala
+++ src/main/scala/com/ryanair/hackathon/hotOffers/users/model/UserDetails.scala
@@ -6,10 +6,10 @@ package com.ryanair.hackathon.hotOffers.users.model
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
-case class UserDetails(userId: String, departureAirport: String, destinationAirports: List[String])
+case class UserDetails(userId: String, departureAirport: String, destinationAirports: List[String], budget: Double)
 
 case object UserSaved
 
 object UsersJson extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val userDetailsJson: RootJsonFormat[UserDetails] = jsonFormat3(UserDetails)
+  implicit val userDetailsJson: RootJsonFormat[UserDetails] = jsonFormat4(UserDetails)
 }
